9226b7e74962c5ed9b10e99a817ef795d29648ce
Durst <sd067616@ohio.edu>
2020-10-06 13:03:28 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CentricProject_Team10.Startup))]
+namespace CentricProject_Team10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
