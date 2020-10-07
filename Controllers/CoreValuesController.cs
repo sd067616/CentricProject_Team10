425050590c38528ad7baabbf7c4ad269d70d6d4a
Samuel Durst <sd067616@ohio.edu>
2020-10-07 15:07:58 -0400

    - added a Core Values page, as well as add a Core Values option to our navbar

---
+++ Controllers/CoreValuesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CentricProject_Team10.Controllers
+{
+    public class CoreValuesController : Controller
+    {
+        // GET: CoreValues
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
