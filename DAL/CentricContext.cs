8e31e953a9d791db7831db216338729fb5153357
Vincent Murray <vm664816@ohio.edu>
2020-10-20 18:09:44 -0400

    Proflie Update

---
+++ DAL/CentricContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CentricProject_Team10.Models;
+
+namespace CentricProject_Team10.DAL
+{
+    public class CentricContext : DbContext
+    {
+            public CentricContext() : base ("nameDefaultConnection")
+            {
+
+            }
+        public DbSet<userData> userData { get; set; }
+
+    }
+    }
---
+++ Models/userData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentricProject_Team10.Models
+{
+    public class userData
+    {
+        public Guid ID { get; set; }
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string fullName
+        {
+            get
+            {
+                return lastName + ", " + firstName;
+            }
+        }
+        public string officeLocation { get; set; }
+        public string position { get; set; }
+        public DateTime startDate { get; set; }
+    }
+}
