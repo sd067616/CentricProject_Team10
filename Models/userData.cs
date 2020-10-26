254357793e5c43d4abcdefe632f1a8cbf8bbe419
Vincent Murray <vm664816@ohio.edu>
2020-10-26 14:05:53 -0400

    View changes

--- Models/userData.cs
+++ Models/userData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace CentricProject_Team10.Models
     public class userData
     {
         public Guid ID { get; set; }
+        [Display(Name = "Last Name")]
         public string lastName { get; set; }
+        [Display(Name = "First Name")]
         public string firstName { get; set; }
+        [Display(Name = "Full Name")]
         public string fullName
         {
             get
@@ -17,8 +21,11 @@ namespace CentricProject_Team10.Models
                 return lastName + ", " + firstName;
             }
         }
+        [Display(Name = "Office Location")]
         public string officeLocation { get; set; }
+        [Display(Name = "Position Description")]
         public string position { get; set; }
+        [Display(Name = "First Day of Employment")]
         public DateTime startDate { get; set; }
     }
 }
