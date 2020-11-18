1b2d89e704cc0535026836b59063308ea5838efb
Samuel Durst <sd067616@ohio.edu>
2020-11-18 18:01:59 -0500

    Commit

--- Controllers/userRecognitionsController.cs
+++ Controllers/userRecognitionsController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using CentricProject_Team10.DAL;
